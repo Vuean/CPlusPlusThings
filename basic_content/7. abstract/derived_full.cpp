8411aed95dca7781206e8a19f4812fcc08665b26
Vuean <hzhlxy@outlook.com>
2020-08-05 22:28:02 +0800

    补充abstract中derived_full.cpp文件

--- basic_content/7. abstract/derived_full.cpp
+++ basic_content/7. abstract/derived_full.cpp
@@ -1,4 +1,31 @@
 /*
 derived_full.cpp
-
+完整示例！抽象类由派生类继承实现！
 */
+
+#include <iostream>
+using namespace std;
+
+class Base 
+{
+public:
+    virtual void func() = 0;
+    int getX() { return x; }
+private:
+    int x;
+};
+
+class Derived : public Base
+{
+public:
+    void func() { cout << "func() called"; }
+private:
+    int y;
+};
+
+int main()
+{
+    Derived d;
+    d.func();
+    return 0;
+}

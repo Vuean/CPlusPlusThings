0f9bc713647c92b5f1d89af13e21db13809a66fb
Vuean <hzhlxy@outlook.com>
2020-08-11 08:17:05 +0800

    feat(16&17):新增17，完善16;

---
+++ basic_content/16. union/union.cpp
@@ -0,0 +1,54 @@
+/*
+union.cpp
+*/
+
+#include <iostream>
+
+// 默认访问控制符为public
+
+union UnionTest {
+    // 可以含有构造函数、析构函数
+    UnionTest() : i(10)
+    {
+        print(i);
+    }
+    ~UnionTest() {}
+    int i;
+
+private:
+    void print(int i)
+    {
+        std::cout << "print: " << i << std::endl;
+    }
+};
+
+// 全局匿名联合必须是静态的
+static union
+{
+    int i;
+    double d;
+};
+
+int main()
+{
+    UnionTest u;
+
+    union
+    {
+        int i;
+        int d;
+    };
+
+    // 输出 UnionTest 联合的 10
+    std::cout << u.i << std::endl;
+
+    ::i = 20;
+    // 输出全局静态匿名联合的 20
+    std::cout << ::i << std::endl;
+
+    // 匿名union在定义所在作用域可直接访问union成员
+    i = 30;
+    std::cout << i << std::endl;    // 输出局部匿名联合的 30
+
+    return 0;
+}
